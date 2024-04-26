8f866fdb1216afeaefe218467a51c3abe27d3c50
fanhailu1 <fanhai@google.com>
2024-04-26 22:40:12 +0000

    change ray

--- jetstream_pt/ray_engine_worker.py
+++ jetstream_pt/ray_engine_worker.py
@@ -789,4 +789,33 @@ class PyTorchEngineRayWorker:
  
   
 
-  
+  # pylint: disable-next=all
+  def load_params(self) -> Params:
+    # We want to fix this: load from files
+    with jax.default_device(self.colocated_cpus):
+      if self.env.checkpoint_path:
+        if self.env.checkpoint_format == "safetensors":
+          return self._load_from_safetensors(self.env.checkpoint_path)
+      else:
+        jax_weights = self._make_state_dict_jax(self.pt_model.state_dict())
+    jax_weights = {
+        key: jax.device_put(value, self.sharding_by_name(key))
+        for key, value in jax_weights.items()
+    }
+    for k, v in jax_weights.items():
+      if k.startswith("layers") and not k.startswith("layers.0"):
+        continue
+      print(f"Name: {k}, shape: {v.shape} x {v.dtype}")
+    return jax_weights
+
+  @property
+  def colocated_cpus(self) -> Union[list[engine_api.CpuDevices], None]:
+    """cpu device"""
+    return jax.devices("cpu")[0]
+
+
+
+
+
+
+ 
